b979ef5fcf80a870e010ba25c05b0270ef11683d
404 <aceboy50@gmail.com>
2025-01-02 00:27:19 +0700

    device: Add support for ASUS RT-AX54
    The ASUS RT-AX54 has the alternative names of AX1800S and AX1800HP, which are used for marketing purpose.
    Till now NMBM hasn't worked yet, so BACKUP everything under asuswrt SSH and STRICTLY flash the firmware under Xiaomi Mi Router 3G's old Breed Bootloader ONLY (with xiaomi.r3g.bootfw=2 or autoboot.command=0x600000).

---
+++ trunk/configs/boards/RT-AX54/board.h
@@ -0,0 +1,19 @@
+#define BOARD_PID		"RT-AX54"
+#define BOARD_NAME		"RT-AX54"
+#define BOARD_DESC		"ASUS RT-AX54"
+#define BOARD_VENDOR_NAME	"ASUSTeK Computer Inc."
+#define BOARD_VENDOR_URL	"http://www.asus.com/"
+#define BOARD_MODEL_URL		"http://www.asus.com/"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+
+#define BOARD_HAS_5G_11AC		1
+#define BOARD_HAS_5G_11AX		1
+#define BOARD_HAS_2G_11AX		1
+#define BOARD_NUM_ANT_5G_TX		2
+#define BOARD_NUM_ANT_5G_RX		2
+#define BOARD_NUM_ANT_2G_TX		2
+#define BOARD_NUM_ANT_2G_RX		2
+#define BOARD_NUM_ETH_LEDS		0
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
